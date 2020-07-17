7849c55c15b4e550796a70d0857bb5b33ab7cafd
hyojaekim <hyojaedev@gmail.com>
2020-07-18 00:28:04 +0900

    feat: Apply current scene

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 (() => {
+
+    let yOffset = 0; //window.pageYOffset 대신 사용
+    let prevScrollHeight = 0; //현재 스크롤 위치보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
+    let currentScene = 0; //현재 활성화된 씬(눈으로 보고 있는 씬)
+
     const sceneInfo = [
         {
             type: 'sticky',
@@ -41,7 +46,26 @@
         }
     }
 
+    function scrollLoop() {
+        prevScrollHeight = 0;
+        for (let i = 0; i < currentScene; i++) {
+            prevScrollHeight += sceneInfo[i].scrollHeight;
+        }
+
+        if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+            currentScene++;
+        }
+
+        if (yOffset < prevScrollHeight && currentScene !== 0) {
+            currentScene--;
+        }
+    }
+
     window.addEventListener('resize', setLayout);
-    
+    window.addEventListener('scroll', () => {
+        yOffset = window.pageYOffset;
+        scrollLoop();
+    });
+
     setLayout();
 })();
